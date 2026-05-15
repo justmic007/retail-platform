5efab4563c7f2d45f30bdfb4589478c8dd59ce5e
Micah J <justmic007@github.com>
2026-05-15 14:17:36 +0100

    removed render related deployment files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building Auth Service..."
-cd backend
-go mod download
-go build -tags netgo -ldflags '-s -w' -o ../app ./services/auth/cmd/server/...
-echo "Build complete!"
